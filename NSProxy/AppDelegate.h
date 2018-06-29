cd43c0097f779a65b71b6eccf07474760e6a06ed
Suxiaoya <sulijun@xmfen.cn>
2018-06-29 16:25:50 +0800

    Initial Commit

---
+++ NSProxy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSProxy
+//
+//  Created by mc on 2018/6/29.
+//  Copyright © 2018年 苏丽君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSProxy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSProxy
+//
+//  Created by mc on 2018/6/29.
+//  Copyright © 2018年 苏丽君. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
